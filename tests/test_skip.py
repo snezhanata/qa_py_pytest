d6776d56789c3757dfd1dbb1dac55a2eb6e4da7f
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-10-21 20:08:25 +0400

    width and height were changed

--- tests/test_skip.py
+++ tests/test_skip.py
@@ -1,27 +1,34 @@
+
 """
-Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
+Переопределите параметр с помощью indirect
 """
 import pytest
+from selene import have
 from selene.support.shared import browser
 
 
-@pytest.fixture()
-def browser_management():
-    browser.config.window_width = 1011
-    browser.config.window_height = 785
+desktop_width = 785
+desktop_height = 1044
+mobile_width = 500
+mobile_height = 785
+
+
+@pytest.fixture(params=[(desktop_width, desktop_height)])
+def browser_management(request):
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
     browser.open('https://github.com')
     yield
 
 
 def test_github_desktop(browser_management):
-    if browser.config.window_width <= 1011:
-        pytest.skip(reason='Browser window size is for mobile')
     browser.element('.HeaderMenu-link--sign-in').click()
-    browser.config.hold_browser_open = True
+    browser.element('.auth-form-header').should(have.text('Sign in to GitHub'))
 
 
+@pytest.mark.parametrize('browser_management', [(mobile_width, mobile_height)], indirect=True, ids=['Changed to mobile browser window size'],
+)
 def test_github_mobile(browser_management):
-    if browser.config.window_width > 1011:
-        pytest.skip(reason='Browser window size is for desktop')
     browser.element('.HeaderMenu-toggle-bar').click()
     browser.element('.HeaderMenu-link--sign-in').click()
+    browser.element('.auth-form-header').should(have.text('Sign in to GitHub'))
