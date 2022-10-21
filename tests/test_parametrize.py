05c2bc33966c4678b74200d756998be79be3dee8
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-10-21 20:38:07 +0400

    width and height were changed

--- tests/test_parametrize.py
+++ tests/test_parametrize.py
@@ -6,13 +6,11 @@ import pytest
 from selene import have
 from selene.support.shared import browser
 
-desktop_width = 785
-desktop_height = 1044
-mobile_width = 500
-mobile_height = 785
+mobile = (500, 650)
+desktop = (1044, 785)
 
 
-@pytest.fixture(params=[(desktop_width, desktop_height)])
+@pytest.fixture(params=[desktop])
 def browser_management(request):
     browser.config.window_width = request.param[0]
     browser.config.window_height = request.param[1]
@@ -25,7 +23,11 @@ def test_github_desktop(browser_management):
     browser.element('.auth-form-header').should(have.text('Sign in to GitHub'))
 
 
-@pytest.mark.parametrize('browser_management', [(mobile_width, mobile_height)], indirect=True, ids=['Changed to mobile browser window size'],
+@pytest.mark.parametrize(
+    'browser_management',
+    [mobile],
+    indirect=True,
+    ids=['Changed to mobile browser window size'],
 )
 def test_github_mobile(browser_management):
     browser.element('.HeaderMenu-toggle-bar').click()
